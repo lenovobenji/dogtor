91e8759b293c94db72722388394fe3e6ba235bcf
Benji <benjifran27@gmail.com>
2023-09-11 16:20:21 -0600

    vets

--- dogtor/vet/urls.py
+++ dogtor/vet/urls.py
@@ -1,13 +1,27 @@
-# .path
-# 2 urlspaterns
+# 1. path
+# 2. urlpatterns
 
 from django.urls import path
 
 # Views
-from .views import list_pet_owners,Test, OwnersList
+# PascalCase -> clases -> modelos o vistas clase
+# snake_case -> para todo lo demas
+from .views import list_pet_owners, Test, OwnersList, OwnerDetail, PetsList, PetsDetail
 
-urlpatterns = [ 
-path("owners/", OwnersList.as_view()),
-path("test/",Test.as_view()),
-     
+# alias (reversed urls) -> rutas de una app en especifico
+# alias (reversed urls) -> rutas del proyecto
+
+# si no tienes include -> reversed url se pone como 3er parametro ejemplo -> name="owners_list"
+# si SI tienes include -> reversed url se pone como 2do parametro DENTRO del include() -> include(("vet.urls", "vet"))
+
+# SINTAXIS de como acceder
+# "vet:owners_list"
+# "vet:owners_detail"
+# "reversed_url_app: reversed_url_singular"
+urlpatterns = [
+    path("owners/", OwnersList.as_view(), name="owners_list"),
+    path("owners/<int:pk>/", OwnerDetail.as_view(), name="owners_detail"),
+    path("pets/", PetsList.as_view()),
+    path("pets/<int:pk>/", PetsDetail.as_view()),
+    path("test/", Test.as_view()),
 ]
--- dogtor/vet/views.py
+++ dogtor/vet/views.py
@@ -1,48 +1,104 @@
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
-from django.views.generic import View, TemplateView, ListView
+from django.views.generic import View, TemplateView, ListView, DetailView
 
-# Create your views here.
 
 # Models
-from vet.models import PetOwner
+from vet.models import PetOwner, Pet
 
+
+# Create your views here.
 def list_pet_owners(request):
-    """lis"""
-    owners = PetOwner.objects.all() 
-    
+    # por defecto -> metodo GET
+    print("REQUEST --->", request.__dict__)
+
+    """List owners."""
+
+    # Agarrar informacion de nuestra bd
+    owners = PetOwner.objects.all()
+
+    # Hacemos el contexto
     context = {"owners": owners}
-    
-    
+
+    # Agarrar template
     template = loader.get_template("vet/owners/list.html")
-    
-    
+
+    # Retornar respuesta HTTP con el template pasandole el contexto y el request
     return HttpResponse(template.render(context, request))
 
 
-class OwnersList (TemplateView):
-      # Renderizane esta clasee
-    model = PetOwner
+# CRUD
+
+
+# Ctrl + /
+# Cmd + /
+class OwnersList(TemplateView):
+    # Renderizame este template
     template_name = "vet/owners/list.html"
-    context_objects_name ="owners"
-    
-    
-      # Que este template va a tener cierto 'contexto'
-    
+
+    # Que este template va a tener cierto 'contexto'
     def get_context_data(self, **kwargs):
-         
+        # Agarrar el contexto que ya creo 'TemplateView'
         context = super().get_context_data(**kwargs)
-         
+
+        # Le agregamos nuestro custom context
         context["owners"] = PetOwner.objects.all()
-         
-        return context          
+        return context
+
+
+class PetsList(TemplateView):
+    # Renderizame este template
+    template_name = "vet/pets/list.html"
+
+    # Que este template va a tener cierto 'contexto'
+    def get_context_data(self, **kwargs):
+        # Agarrar el contexto que ya creo 'TemplateView'
+        context = super().get_context_data(**kwargs)
+
+        # Le agregamos nuestro custom context
+        context["pets"] = Pet.objects.all()
+        return context
+
+
+class PetsDetail(TemplateView):
+    # Renderizame este template
+    template_name = "vet/pets/detail.html"
+
+    # Que este template va a tener cierto 'contexto'
+    def get_context_data(self, **kwargs):
+        # Agarrar el contexto que ya creo 'TemplateView'
+
+        context = super().get_context_data(**kwargs)
+
+        # Le agregamos nuestro custom context
+        context["pet"] = Pet.objects.get(pk=kwargs["pk"])
+        return context
+
+
+class OwnersList(ListView):
+    """Render all Pet Owners."""
+
+    # 1. Modelo que estamos manipulando
+    # 2. Template con el que vamos renderizar
+    # 3. El contexto que va a tener ese template
+    model = PetOwner  # 1
+    template_name = "vet/owners/list.html"  # 2
+    context_object_name = "owners"  # 3
+
+
+class OwnerDetail(DetailView):
+    """Render a specific Pet Owner with their pk."""
+
+    # 1. Modelo
+    # 2. Template a renderizar
+    # 3. El contexto que va a tener ese template
+    model = PetOwner
+    template_name = "vet/owners/detail.html"
+    context_object_name = "owner"
+
 
 class Test(View):
-    # como funcion el metodo de (get,pats, post, delate)
-    
+    # Como funcion el metodo(GET,PATCH,POST,DELETE,PUT)
     def get(self, request):
-        return HttpResponse("Hello word from a class generation view")
-    
-    
-    
+        return HttpResponse("Hello world from a class generic view")
